5bfbff6f4c7461e2a3386eff38c9179813f1c5b6
christinacarlson <christina.carlson213@gmail.com>
2018-07-11 19:19:19 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,22 @@
-#write out your code here
+# user imputs amount of coins 
+puts "How many cents do you have?"
+cents = gets.strip.to_i
+
+# if its > 25 take out 25 and add quarter to bucket
+
+
+# until its < 25
+# if > 10 take out 10 and add a dime to bucket
+# until its < 10
+# if > 5 take out 5 and add a nickel to bucket
+# until < 5
+# if > 1 take out 1 and add a penny to bucket
+# until == 0
+# spit out bucket
+
 
 def least_coins(cents)
 
-#Code your answer here!
+
 
 end
